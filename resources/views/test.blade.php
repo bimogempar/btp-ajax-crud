f8818fb505ec1225fe93e8f59ccc5c9c904d6098
bimogempar <bimogempar@gmail.com>
2022-04-02 17:48:08 +0700

    first setup modal using attribute

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -13,6 +13,54 @@
 
 <body>
     <div class="m-5">
+
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Create New Event
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Create New Method and Event</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="method">Which Method Exist</label>
+                            <select class="form-select" name="method" id="">
+                                <option disabled selected>Open this select menu</option>
+                                @foreach ($methods as $method)
+                                    <option value={{ $method->id }}>{{ $method->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="method">Method</label>
+                            <input type="text" class="form-control" id="method" placeholder="Create new method">
+                        </div>
+                        <div class="mb-3">
+                            <label for="event">Event</label>
+                            <input type="text" class="form-control" id="event" placeholder="Create new event">
+                        </div>
+                        <div class="mb-3">
+                            <label for="start_date">Start Date</label>
+                            <input type="date" class="form-control" id="start_date">
+                        </div>
+                        <div class="mb-3">
+                            <label for="end_date">End Date</label>
+                            <input type="date" class="form-control" id="end_date">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -32,7 +80,9 @@
                             <td>
                                 @foreach ($method->events as $event)
                                     @if ($date === date('F', strtotime($event->start_date)))
-                                        <li>{{ $event->name }} <br>
+                                        <li type="button" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal{{ $event->id }}">
+                                            {{ $event->name }} <br>
                                             <span class="text-primary">({{ $event->start_date }} -
                                                 {{ $event->end_date }})</span>
                                         </li>
@@ -48,6 +98,34 @@
                 </tr>
             </tbody>
         </table>
+
+        {{-- modal using attribute --}}
+        @foreach ($methods as $method)
+            @foreach ($method->events as $event)
+                <div class="modal fade" id="exampleModal{{ $event->id }}" tabindex="-1"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">{{ $method->name }}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                {{ $event->name }} <br>
+                                <span class="text-primary">({{ $event->start_date }} -
+                                    {{ $event->end_date }})</span>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endforeach
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
